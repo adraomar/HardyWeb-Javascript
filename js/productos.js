737b6c3eebe0f68b606bbae536ed381630f9b8de
adraomar <adra.omar2@gmail.com>
2022-07-09 17:00:14 -0300

    Eliminado producto.js y agregado archivo producto-data.json para consumir desde api con fetch

--- js/productos.js
+++
@@ -1,5 +0,0 @@
-let stockProductos = [
-    {id: 1, nombre:"Procesador i3", tipo:"procesadores", precio: 1500, img: 'https://www.profesionalreview.com/wp-content/uploads/2020/10/i3-10-generacion.jpg'},
-    {id: 2, nombre:"Procesador i5", tipo:"procesadores", precio: 3000, img: 'https://bigpoint.com.ar/wp-content/uploads/2020/05/Procesador-Intel-i5-10400-10%E1%B5%90%E1%B5%83-Generaci%C3%B3n.jpg'},
-    {id: 3, nombre:"Procesador i7", tipo:"procesadores", precio: 4500, img: 'https://bigpoint.com.ar/wp-content/uploads/2020/06/Procesador-Intel-i7-10700.jpg'}
-];
