45a46794352aa38cdb086064666afcc13f235c7f
adraomar <adra.omar2@gmail.com>
2022-07-13 02:10:04 -0300

    Modificado lo visual al ingresar datos de usuario

--- js/login.js
+++ js/login.js
@@ -1,7 +1,6 @@
 const botonIngresar = document.getElementById("btnIngresar");
 const botonRegistrar = document.getElementById("btnRegistrar");
-const textUsuario = document.getElementById("txtUsuario");
-const textPassword = document.getAnimations("txtPassword");
+const contenedorBotonesIngreso = document.getElementById("ingreso-registro");
 
 botonIngresar.addEventListener('click', () => {
     Swal.fire({
@@ -10,7 +9,7 @@ botonIngresar.addEventListener('click', () => {
             '<form>' +
             '<div class="mt-3 mb-3 text-start">' +
             '<label for="txtUsuario" class="form-label">Nombre de usuario: </label>' +
-            '<input type="text" class="form-control" id="txtUsuario" placeholder="Nombre de usuario...">' +
+            '<input type="text" class="form-control" id="txtUsuario" placeholder="Nombre de usuario">' +
             '</div>' +
             '<div class="mb-3 text-start">' +
             '<label for="txtPassword" class="form-label">Contraseña: </label>' +
@@ -38,6 +37,13 @@ botonIngresar.addEventListener('click', () => {
                 text: 'Te has conectado con las credenciales correctamente.',
                 confirmButtonText: 'Cerrar'
             })
+            contenedorBotonesIngreso.removeChild(botonIngresar);
+            contenedorBotonesIngreso.removeChild(botonRegistrar);
+            
+            let h3 = document.createElement('h3');
+            h3.className = 'h3 text-light';
+            h3.innerText = `${result.value.login}`;
+            contenedorBotonesIngreso.appendChild(h3);
         }
         else {    
             Swal.fire({
