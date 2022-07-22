15da571cd362e1b22032a0df3234c625353ebe8a
adraomar <adra.omar2@gmail.com>
2022-07-22 01:54:16 -0300

    Checkout lee carrito y muestra detalles del mismo

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,17 +1,56 @@
-(() => {
-    'use strict'
+let carrito = JSON.parse(localStorage.getItem("CarritoDeCompras"));
 
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
+(() => {
+  'use strict'
+
+  const forms = document.querySelectorAll('.needs-validation')
+
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
     })
-  })()
+  })
   
+})()
+
+function mostrarCarrito() {
+
+  let ul = document.getElementById("tu-carrito");
+  let tituloCarrito = document.getElementById("titulo-carrito");
+
+  let span = document.createElement("span");
+  span.className = "badge bg-primary rounded-pill";
+
+  let contador = 0;
+  let precioTotal = 0;
+
+  carrito.forEach(producto => {
+    ul.innerHTML += `
+    <li class="list-group-item d-flex justify-content-between lh-sm">
+      <div>
+        <h6 class="my-0">${producto.title}</h6>
+        <small class="text-muted">${producto.category}</small>
+      </div>
+      <span class="text-muted">$ ${producto.price.toLocaleString('de-DE')}</span>
+    </li>`;
+
+    contador++;
+    precioTotal += producto.price;
+  });
+
+  ul.innerHTML += `
+  <li class="list-group-item d-flex justify-content-between bg-dark text-white">
+    <span>Total (USD)</span>
+    <strong>$ ${precioTotal.toLocaleString('de-DE')}</strong>
+  </li>`;
+
+  span.innerText = contador;
+
+  tituloCarrito.appendChild(span);
+
+}
